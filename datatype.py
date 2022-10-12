c7780ef818e7f57e456776826fd15f25ca61add6
Kiash254 <codewithkiash@gmail.com>
2022-10-13 02:33:43 +0300

    added

---
+++ datatype.py
@@ -0,0 +1,18 @@
+#show all data types all python
+num=22
+print('this is and integer',num)
+
+name='my name is a web developer'
+print('am a web developer with python backend',name)
+
+a=2.5
+print('this is a float',a)
+
+#string concatenation with integer
+myname='am a web dev with python web framework'
+age=22
+
+
+who_i_am=f"{myname} and i am {age} years old"
+
+print(who_i_am)
