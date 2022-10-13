17d95aad6692ddfc1f7290cdfb6365c57c67072e
Kiash254 <codewithkiash@gmail.com>
2022-10-13 09:38:43 +0300

    added

---
+++ list.py
@@ -0,0 +1,8 @@
+mylist=['data science','Game dev','Python','C++','Java']
+print("this is a list of my favorite programming languages",mylist)
+
+print('the first item in the list is',mylist[0])
+print("The length of the list is",len(mylist))
+
+mixed_list=['data science','Game dev','Python','C++','Java',1,2,3,4,5]
+print("My mixed list is",mixed_list)
