12407096651eeec724530ec51d22016a4c6ca601
Kiash254 <codewithkiash@gmail.com>
2022-10-26 07:47:01 +0300

    added some eams csv files

--- data.sql
+++ data.sql
@@ -18,6 +18,9 @@ insert into students values(6, 'Alice', 40);
 --add a column to the table to hold the average grade of the student
 alter table students add average_grade int;
 
+
+--delete a column students tables
+
 --update the average grade of the student
 update students set average_grade = (grade + 10) / 2;
 --and add data to the new column
@@ -40,3 +43,28 @@ create table user(
     name varchar(50) primary key,
     role varchar(50)
 );
+--create  tables employee and hold username,age,details,department,role,working hours and clossing time
+create table employee(
+    username varchar(50) primary key,
+    age int,
+    details varchar(50),
+    department varchar(50),
+    role varchar(50),
+    working_hours int,
+    closing_time int
+);
+--drop table employee;
+--insert data into the table
+insert into employee values('admin', 20, 'admin', 'admin', 'admin', 8, 5);
+insert into employee values('user', 20, 'user', 'user', 'user', 8, 5);
+insert into employee values('guest', 20, 'guest', 'guest', 'guest', 8, 5);
+--add a column to the table to hold the sign in time of the user in employee table
+alter table employee add sign_in_time int;
+--insert data into the table of employee
+insert into employee values('admin', 20, 'admin', 'admin', 'admin', 8, 5, 8);
+insert into employee values('user', 20, 'user', 'user', 'user', 8, 5, 8);
+insert into employee values('guest', 20, 'guest', 'guest', 'guest', 8, 5, 8);
+--add a column to the table to hold the sign out time of the user in employee table
+alter table employee add sign_out_time int;
+
+
---
+++ finix.sql
@@ -0,0 +1,12 @@
+-- create table called karatina library  to hold the borrower,book,author,publisher 
+create table karatina_library(
+    borrower varchar(50) primary key,
+    book varchar(50),
+    author varchar(50),
+    publisher varchar(50)
+);
+--insert data into the table
+insert into karatina_library values('admin', 'admin', 'admin', 'admin');
+insert into karatina_library values('user', 'user', 'user', 'user');
+insert into karatina_library values('guest', 'guest', 'guest', 'guest');
+--add a column to the table to hold the sign in time of the user in karatina library table
