897d44f93c6b38f0932968058d3501c53c9af6e3
Kiash254 <codewithkiash@gmail.com>
2022-11-01 22:17:41 +0300

    added added karatina university with one table called borrower

--- karatina.sql
+++ karatina.sql
@@ -1,1 +1,14 @@
---create schema called karatina university with the following tables a
+--create schema called karatina university database 
+CREATE DATABASE karatina;
+USE karatina;
+--create a table called Borrower with the following fields borrower_id, borrower_name, borrower_address, borrower_phone, borrower_email, borrower_password, borrower_status and borrower_id set as primary key
+CREATE TABLE Borrower(
+borrower_id INT NOT NULL AUTO_INCREMENT,
+borrower_name VARCHAR(50) NOT NULL,
+borrower_address VARCHAR(50) NOT NULL,
+borrower_phone VARCHAR(50) NOT NULL,
+borrower_email VARCHAR(50) NOT NULL,
+borrower_password VARCHAR(50) NOT NULL,
+borrower_status VARCHAR(50) NOT NULL,
+PRIMARY KEY(borrower_id)
+);
