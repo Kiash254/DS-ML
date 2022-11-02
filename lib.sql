b0b4884224cae131bc0b995b25c636ccb1849ff7
Kiash254 <codewithkiash@gmail.com>
2022-11-02 09:35:21 +0300

    added on lib sql added foreigh keys

--- lib.sql
+++ lib.sql
@@ -19,29 +19,33 @@ INSERT INTO books(book_name,author,publisher) VALUES
 ('The 4-Hour Workweek','Timothy Ferriss','Crown Business'),
 ('The 4-Hour Body','Timothy Ferriss','Crown Business'),
 ('The 4-Hour Chef','Timothy Ferriss','Crown Business');
---create a table called borrower with the following fields borrower_id,borrower_name,book_id and set borrower_id as a primary key and set borrower_id as an auto increment
+--create a table called borrower with the following fields borrower_id,borrower_name,book_id and set borrower_id as a primary key and set borrower_id as an auto increment add foreign key to book_id
 CREATE TABLE borrower(
 borrower_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 borrower_name VARCHAR(30) NOT NULL,
-book_id INT NOT NULL
+book_id INT NOT NULL,
+FOREIGN KEY (book_id) REFERENCES books(book_id)
 );
---add 10 records to the table borrower
+
+--add 10 records to the table borrower 
 INSERT INTO borrower(borrower_name,book_id) VALUES
 ('John',1),
-('Mary',2),
-('Peter',3),
-('James',4),
-('Sarah',5),
-('John',6),
-('Mary',7),
+('Peter',2),
+('James',3),
+('John',4),
+('Peter',5),
+('James',6),
+('John',7),
 ('Peter',8),
-('Jane',9),
-('James',10);
---create a table called author with the following fields author_id,author_name,book_id and set author_id as a primary key and set author_id as an auto increment
+('James',9),
+('John',10);
+
+--create a table called author with the following fields author_id,author_name,book_id and set author_id as a primary key and set author_id as an auto increment add foreign key to book_id
 CREATE TABLE author(
 author_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 author_name VARCHAR(30) NOT NULL,
-book_id INT NOT NULL
+book_id INT NOT NULL,
+FOREIGN KEY (book_id) REFERENCES books(book_id)
 );
 --add 10 records to the table author
 INSERT INTO author(author_name,book_id) VALUES
@@ -54,12 +58,13 @@ INSERT INTO author(author_name,book_id) VALUES
 ('Timothy Ferriss',7),
 ('Timothy Ferriss',8),
 ('Timothy Ferriss',9),
-('Gary Chapman',10);
---create a table called publisher with the following fields publisher_id,publisher_name,book_id and set publisher_id as a primary key and set publisher_id as an auto increment
+('Timothy Ferriss',10);
+--create a table called publisher with the following fields publisher_id,publisher_name,book_id and set publisher_id as a primary key and set publisher_id as an auto increment, add foreign key to book_id
 CREATE TABLE publisher(
 publisher_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 publisher_name VARCHAR(30) NOT NULL,
-book_id INT NOT NULL
+book_id INT NOT NULL,
+FOREIGN KEY (book_id) REFERENCES books(book_id)
 );
 --add 10 records to the table publisher
 INSERT INTO publisher(publisher_name,book_id) VALUES
